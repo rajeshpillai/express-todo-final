0936c22ae2d64a3b19b28cd5f76b620d943c4f04
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-05-10 12:24:27 +0530

    Updated readme

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const process = require("process");
 const layout = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
@@ -16,8 +17,6 @@ app.use(layout);
 app.use("/public", express.static(__dirname + "/public"));
 app.use("/bower_components", express.static(__dirname +  "/bower_components"));
 
-
-
 var port = 8888;
 
 app.use(bodyParser.json());
@@ -28,7 +27,6 @@ app.use(bodyParser.urlencoded({
 
 
 var db;
-
 var requestCounter = 0;
 
 var liveurl = "mongodb://mongouser:mongouser123@ds019668.mlab.com:19668/raj-todos";
@@ -47,17 +45,13 @@ MongoClient.connect(url, (err, database) => {
     if (err) return console.log(err);
     db = database;
 
-    // stub todos
-    //stubTodos();
-
     app.listen(port, function () {
         console.log(`Listening on port ${port}`);
     });
-
 });
 
 app.use(function (req, res, next) {
-  console.log("LOGGER: ", __dirname);
+  console.log("LOGGER: ", process.pid);
   req.db = db;
   next();
 })
@@ -94,47 +88,3 @@ function stubTodos () {
 }
 
 require("./server/router")(app);
-
-app.get("/edit/:id", function (req, res) {
-    console.log("editing todo with id: ", req.params.id);
-
-    var id = new mongodb.ObjectID(req.params.id);
-
-     db.collection("todos").find({_id: id}).toArray().then(function (data){
-        console.log("todo: ", data[0]);
-        var todo = data[0];
-         res.render("edit.ejs", {todo: todo});
-    });
-
-});
-
-
-
-app.post("/deleteall", function (req, res) {
-  console.log("REQUEST: deleteAll");
-  db.collection("todos").deleteMany();
-  res.end("ok");
-});
-
-app.post("/update", function (req, res) {
-    var id = new mongodb.ObjectID(req.body.id);
-    console.log("udpate: ", req.body.todo);
-    db.collection("todos")
-        .update({_id:id},{$set: {name:req.body.name, todo:req.body.todo}},
-            function(err, result){
-             });
-
-    res.redirect("/");
-});
-
-// create todo
-app.post('/todos', (req, res) => {
-    console.log("Creating todo: ", req.body);
-    req.body.createdAt = new Date();
-    req.body.completed = false;
-    db.collection("todos").save(req.body, (err, result) => {
-        if (err) return console.log(err);
-        console.log("Saved to the database!");
-        res.redirect("/");
-    });
-});
